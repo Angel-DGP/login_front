410116a15b95f2273278283dd40d88c327d3c839
Angel-DGP <angel.patinogd@gmail.com>
2025-05-13 12:25:04 -0500

    REgresar bien

--- assets/screens/App_RegisterNav.js
+++ assets/screens/App_RegisterNav.js
@@ -1,49 +1,62 @@
 import { useState } from "react";
-import { View, Text, StyleSheet, FlatList, Alert, TextInput } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  Alert,
+  TextInput,
+} from "react-native";
 import { Button } from "react-native-web";
 import { insertUser } from "../../rest_client/rest";
 export const RegisterNav = ({ navigation, route }) => {
   const handleRegister = async () => {
-  const result = await insertUser(name, password, email);
-  if (result) {
-    Alert.alert("Registro exitoso", `Usuario creado: ${result.name_user}`);
-  } else {
-    Alert.alert("Error", "No se pudo registrar el usuario.");
-  }
-};
-    const [name, onChangeName] = useState("");
-    const [password, onChangePassword] = useState("");
-    const [email, onChangeEmail] = useState("");
-    const exists = async () =>{
-      const users = await getAllUsers();
-          console.log("Usuarios obtenidos:", users);
-          const matchedUser = users.find(
-            (user) => user.name_user === name && user.password_user === password
-          );
+    const result = await insertUser(name, password, email);
+    if (result) {
+      Alert.alert("Registro exitoso", `Usuario creado: ${result.name_user}`);
+    } else {
+      Alert.alert("Error", "No se pudo registrar el usuario.");
     }
+  };
+  const [name, onChangeName] = useState("");
+  const [password, onChangePassword] = useState("");
+  const [email, onChangeEmail] = useState("");
+  const exists = async () => {
+    const users = await getAllUsers();
+    console.log("Usuarios obtenidos:", users);
+    const matchedUser = users.find(
+      (user) => user.name_user === name && user.password_user === password && user.email_user === email
+    );
+    if (matchedUser) {
+      console.log("Datos no registrados");
+    } else {
+      
+      navigation.navigate("Login_Nav");
+    }
+  };
   return (
     <View>
       <Text>Register screen</Text>
       <TextInput
-              style={styles.input}
-              onChangeText={onChangeName}
-              value={name}
-              placeholder="Ingrese su usuario"
-            />
-            <TextInput
-              style={styles.input}
-              onChangeText={onChangePassword}
-              value={password}
-              placeholder="Ingrese su contraseña"
-              secureTextEntry
-            />
-            <TextInput
-              style={styles.input}
-              onChangeText={onChangeEmail}
-              value={email}
-              placeholder="Ingrese su correo electronico"
-            />
-            <Button title="Registrarse" onPress={handleRegister}/>
+        style={styles.input}
+        onChangeText={onChangeName}
+        value={name}
+        placeholder="Ingrese su usuario"
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangePassword}
+        value={password}
+        placeholder="Ingrese su contraseña"
+        secureTextEntry
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeEmail}
+        value={email}
+        placeholder="Ingrese su correo electronico"
+      />
+      <Button title="Registrarse" onPress={handleRegister} />
     </View>
   );
 };
